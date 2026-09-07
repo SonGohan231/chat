bb490fa8e6e763053ae8eaa1decd35834f72f94e
SonGohan231 <adambrdon@gmail.com>
2026-09-07 17:40:36 +0200

    Add persistent voice runtime state

---
+++ app/src/main/java/pl/somaskan/questgpt/VoiceAgentRuntime.kt
@@ -0,0 +1,13 @@
+package pl.somaskan.questgpt
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+object VoiceAgentRuntime {
+    var desiredRunning by mutableStateOf(false)
+    var running by mutableStateOf(false)
+    var state by mutableStateOf("Głos wyłączony")
+    var lastError by mutableStateOf<String?>(null)
+    var reconnectAttempt by mutableStateOf(0)
+}

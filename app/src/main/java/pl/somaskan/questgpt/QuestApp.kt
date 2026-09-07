e75c842f208fe739c1b6cbf684f2dc6be708faab
SonGohan231 <adambrdon@gmail.com>
2026-09-07 14:00:20 +0200

    Start adaptive ADB vision with QuestGPT process

--- app/src/main/java/pl/somaskan/questgpt/QuestApp.kt
+++ app/src/main/java/pl/somaskan/questgpt/QuestApp.kt
@@ -2,11 +2,13 @@ package pl.somaskan.questgpt
 
 import android.app.Application
 import android.content.Context
+import pl.somaskan.questgpt.adb.AdbVisionMonitor
 
 class QuestApp : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+        AdbVisionMonitor.start()
     }
 
     companion object {

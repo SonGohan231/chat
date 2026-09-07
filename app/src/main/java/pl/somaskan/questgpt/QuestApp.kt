8398b33e7d96fcffa56107b93cb2e25029ee399c
SonGohan231 <adambrdon@gmail.com>
2026-09-07 15:46:11 +0200

    Start persistent agent service from application

--- app/src/main/java/pl/somaskan/questgpt/QuestApp.kt
+++ app/src/main/java/pl/somaskan/questgpt/QuestApp.kt
@@ -3,12 +3,19 @@ package pl.somaskan.questgpt
 import android.app.Application
 import android.content.Context
 import pl.somaskan.questgpt.adb.AdbVisionMonitor
+import pl.somaskan.questgpt.adb.AgentAccessPolicy
 
 class QuestApp : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
-        AdbVisionMonitor.start()
+        AgentAccessPolicy.refreshRuntime()
+        if (AgentServiceController.isEnabled(this)) {
+            AgentServiceController.start(this)
+        } else {
+            // On-demand capture still works even if the persistent service is disabled.
+            AdbVisionMonitor.stop()
+        }
     }
 
     companion object {
